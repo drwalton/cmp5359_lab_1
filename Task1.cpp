8e2b5bc0d993931f23717a6c1a9d47d7dc3dc575
David R. Walton <drwalton@users.noreply.github.com>
2024-07-27 13:31:04 +0100

    Initial version of tasks

---
+++ Task1.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <lodepng.h>
+
+
+int main()
+{
+	std::string outputFilename = "output.png";
+
+	const int width = 1920, height = 1080;
+	const int nChannels = 4;
+
+	// Setting up an image buffer
+	// This std::vector has one 8-bit value for each pixel in each row and column of the image, and
+	// for each of the 4 channels (red, green, blue and alpha).
+	// Remember 8-bit unsigned values can range from 0 to 255.
+	std::vector<uint8_t> imageBuffer(height*width*nChannels);
+
+	// This for loop sets all the pixels of the image to a cyan colour. 
+	for(int y = 0; y < height; ++y) 
+		for (int x = 0; x < width; ++x) {
+			int pixelIdx = x + y * width;
+			imageBuffer[pixelIdx * nChannels + 0] = 0; // Set red pixel values to 0
+			imageBuffer[pixelIdx * nChannels + 1] = 128; // Set green pixel values to 128 (half brightness)
+			imageBuffer[pixelIdx * nChannels + 2] = 128; // Set blue pixel values to 128 (half brightness)
+			imageBuffer[pixelIdx * nChannels + 3] = 255; // Set alpha (transparency) pixel values to 255 (fully opaque)
+		}
+
+	/// *** Lab Tasks ***
+	// * Task 1: Try adapting the code above to set the upper half of the image to be a red colour.
+	// * Task 2: Doing the maths above to work out indices is a bit annoying! Write your own setPixel function.
+	//           This should take x and y coordinates as input, and red, green, blue and alpha values.
+	//           Remember to pass in your imageBuffer. Should it be passed in by reference or by value? Should
+	//           the reference be const?
+	//           We will use this setPixel function to build our rasteriser in the upcoming labs.
+	//			 Test your setPixel function by setting pixels in your image to different colours.
+	// * Optional Task 3: Use your setPixel function to draw a circle in the centre of the image. Remember a point is
+	//           in a circle if (x - x_0)^2 + (y - y_0)^2 < radius (here x_0, y_0 are the coordinates at the middle of 
+	//           the circle). 
+	//           Hint - use a similar for loop to the one above, and add an if statement to check if the current
+	//           pixel lies in the circle.
+
+
+	// *** Encoding image data ***
+	// PNG files are compressed to save storage space. 
+	// The lodepng::encode function applies this compression to the image buffer and saves the result 
+	// to the filename given.
+	int errorCode;
+	errorCode = lodepng::encode(outputFilename, imageBuffer, width, height);
+	if (errorCode) { // check the error code, in case an error occurred.
+		std::cout << "lodepng error encoding image: " << lodepng_error_text(errorCode) << std::endl;
+		return errorCode;
+	}
+
+	return 0;
+}
---
+++ Task2.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <lodepng.h>
+
+
+int main()
+{
+	std::string inputFilename = "../images/stanford_bunny.png";
+	std::string outputFilename = "output_bunny.png";
+
+	std::vector<uint8_t> imageBuffer;
+	unsigned int width, height;
+	lodepng::decode(imageBuffer, width, height, inputFilename);
+	
+	// *** Tasks ***
+	// This code loads an image from a png file. This is an image of the famous 
+	// Stanford Bunny https://engineering.stanford.edu/magazine/tale-ubiquitous-stanford-bunny
+	// You'll need to load and manipulate images to add texturing to your rasteriser and raytracer.
+	// Let's try changing this image.
+	// If you'd like, you can use the setPixel function you wrote in the previous task.
+	// The code below reduces the brightness of the image by 0.5x, as an example.
+
+	for(int y = 0; y < height; ++y) 
+		for (int x = 0; x < width; ++x) 
+			for (int c = 0; c < 3; ++c) {
+				int pixelIdx = x + y * width;
+				imageBuffer[pixelIdx * 4 + c] *= 0.5;
+			}
+
+	// Try the following:
+	// * Task 1: Try making a *negative* of the input image. Pixels that are bright in the input
+	//           should be dark in your output. 
+	//           Hint: if the pixels ranged in value from 0 to 1, you could replace each pixel value (v) with (1 - v). 
+	//           but remember, the pixels have 8-bit unsigned values, so range from 0 to 255!
+	// * Optional Task 2: Try downsampling this image to 1/2 resolution.
+	//           You can either just keep one in every 4 pixels, or better yet, average the pixels in each 2x2 block.
+	//           Hint: Be careful when averaging! You probably want to convert the pixel values to floating-point to
+	//           do the averaging maths.
+
+	int errorCode;
+	errorCode = lodepng::encode(outputFilename, imageBuffer, width, height);
+	if (errorCode) { // check the error code, in case an error occurred.
+		std::cout << "lodepng error encoding image: " << lodepng_error_text(errorCode) << std::endl;
+		return errorCode;
+	}
+
+	return 0;
+}
